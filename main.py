e12fd1d445fec7b15ccf23b5f9ee61f33a40df2e
Gibbsface <cole.gibbs@gmail.com>
2026-04-22 11:49:24 -0500

    importing argparse

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import argparse
 
 def main():
     print("Hello World!")
+
+    # TODO set up arg parser
+    # let -v be a flag for verbose mode
+    # the first positional argument needs to be a URL
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--verbose", action="store_true")
+    # args = parser.parse_args()
+
+    # if args.verbose:
+    #     print("Verbose mode on")
 
 main()
