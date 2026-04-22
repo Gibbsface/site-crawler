8dadbfed8c415aecd73ba2db94cfa8b3af016c2d
Gibbsface <cole.gibbs@gmail.com>
2026-04-22 11:29:47 -0500

    Initializing testing

---
+++ test/test_main.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestFunctions(unittest.TestCase):
+    def test_text(self):
+        ans1 = 1
+        ans2 = 1
+
+        self.assertEqual(ans1, ans2)
+
+if __name__ == "__main__":
+    unittest.main()
