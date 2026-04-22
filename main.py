eca97bfa9ca28a57beb6b81848c00f6185903a2f
Gibbsface <cole.gibbs@gmail.com>
2026-04-22 15:39:03 -0500

    refactor main

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from code.arg_handler import handle_args
 
 def main():
     args = handle_args()
-    is_v = args.verbose
-    base_url = args.url
 
-    if is_v:
-        print(f"Base URL: {base_url}")
+    if args.verbose:
+        print(f"Base URL: {args.url}")
 
     
 
